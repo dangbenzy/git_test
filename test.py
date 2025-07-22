09ab1d037d29a2ba8ab4e1231ea5fbc42fe6fec8
Gbenga <dangbenzy@gmail.com>
2025-07-22 13:56:22 +0100

    added extra exclamation

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ def main():
     user_input = input("Enter some text: ")
     print(f"You entered: {user_input}")
     print('done')
-    sys.exit('done!')
+    sys.exit('done!!!')
 
 if __name__ == "__main__":
     main()
