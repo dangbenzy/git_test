7e30da776ef0df876530427fb3aa5e3f15c18d32
Ilori Gbenga <56758805+dangbenzy@users.noreply.github.com>
2025-07-22 14:14:42 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import sys
 def main():
     user_input = input("Enter some text: ")
+    print(f"You entered: {user_input}")
+    print('done')
+    sys.exit('done!!!')
 
 if __name__ == "__main__":
     main()
